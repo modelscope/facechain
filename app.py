55c2395e62c5886b05171b5e23d53748c1d4fc8f
yuze.zyz <yuze.zyz@alibaba-inc.com>
2023-08-17 22:50:37 +0800

    revert code

--- app.py
+++ app.py
@@ -230,6 +230,11 @@ def train_input():
                     instance_images = gr.Gallery()
                     upload_button = gr.UploadButton("选择图片上传", file_types=["image"], file_count="multiple")
                     upload_button.upload(upload_file, upload_button, instance_images)
+                    gr.Markdown('''
+                        - Step 1. 上传你计划训练的图片，3~10张头肩照（注意：图片中多人脸、脸部遮挡等情况会导致效果异常，需要重新上传符合规范图片训练）
+                        - Step 2. 点击 [形象定制] ，启动模型训练，等待约15分钟，请您耐心等待
+                        - Step 3. 切换至 [形象体验] ，生成你的风格照片
+                        ''')
 
         run_button = gr.Button('开始训练（等待上传图片加载显示出来再点，否则会报错）')
 
