bd3f71a36255400158756cfb972358fe7877c746
yuze.zyz <yuze.zyz@alibaba-inc.com>
2023-08-17 18:55:53 +0800

    fix script

--- run_inference.py
+++ run_inference.py
@@ -8,10 +8,6 @@ use_main_model = True
 use_face_swap = True
 use_post_process = True
 use_stylization = False
-
-gen_portrait = GenPortrait(use_main_model, use_face_swap, use_post_process,
-                           use_stylization)
-
 processed_dir = './processed'
 num_generate = 5
 base_model = 'ly261666/cv_portrait_model'
@@ -19,7 +15,14 @@ revision = 'v2.0'
 base_model_sub_dir = 'film/film'
 train_output_dir = './output'
 output_dir = './generated'
+cloth_prompt = 'wearing high-class business/working suit'
+style_model_path = None
+multiplier_style = None
+add_prompt_style = None
 
+gen_portrait = GenPortrait(cloth_prompt, style_model_path, multiplier_style, add_prompt_style,
+                           use_main_model, use_face_swap, use_post_process,
+                           use_stylization)
 outputs = gen_portrait(processed_dir, num_generate, base_model,
                        train_output_dir, base_model_sub_dir, revision)
 
