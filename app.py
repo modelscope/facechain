1088d30ab294a5f8b8c936d68ab6846a8ffec4c9
tastelikefeet <58414341+tastelikefeet@users.noreply.github.com>
2023-08-19 00:46:26 +0800

    fix code (#60)

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ def inference_input():
                                        value=HOT_MODELS[0])
                 pos_prompt = gr.Textbox(label="Prompt", lines=3,
                                         value=f'[{cloth_prompt[0]["name"]}]' + generate_pos_prompt(None,
-                                                                                                   cloth_prompt[0]))
+                                                                                                   cloth_prompt[0]['prompt']))
                 style_model = gr.Textbox(label="风格模型(Style model)", value=styles[0]['name'])
 
                 prompts = []
