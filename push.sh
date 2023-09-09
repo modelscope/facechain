c10f62cbd375fb0d59ce86ead036c35f4a934e77
EnlZhao <EnlZhao@163.com>
2023-09-09 21:57:55 +0800

    up

---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git remote set-url origin https://github.com/enl-z/facechain.git
+git add .
+git commit -m 'up'
+git push
+enl-z
+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
