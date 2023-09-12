a510624b9d4efa815af01523a48bbbd5878c4ce2
Tyytyy528 <Tyytyy528@163.com>
2023-09-13 00:42:10 +0800

    Add special_force by tyy

--- facechain/constants.py
+++ facechain/constants.py
@@ -14,7 +14,8 @@ base_models = [
     'revision': 'v1.0.0',
     'sub_path': "realistic",
      # zju_02 add here
-    'style_list': ['水墨1','水墨2','冬季汉服(Chinese winter hanfu)', '校服风(School uniform)', '婚纱风(Wedding dress)', '夜景港风(Hong Kong night style)', '雨夜(Rainy night)', '模特风(Model style)', '机车风(Motorcycle race style)', '婚纱风-2(Wedding dress 2)','拍立得风(Polaroid style)', '仙女风(Fairy style)', '古风(traditional chinese style)', '壮族服装风(Zhuang style)', '欧式田野风(European fields)', '自然户外风(natural outdoor)','武林（gongfu）']},
+    'style_list': ['特种兵01','特种兵02','特种兵03','水墨1','水墨2','冬季汉服(Chinese winter hanfu)', '校服风(School uniform)', '婚纱风(Wedding dress)', '夜景港风(Hong Kong night style)', '雨夜(Rainy night)', '模特风(Model style)', '机车风(Motorcycle race style)', '婚纱风-2(Wedding dress 2)']},
+    #,'拍立得风(Polaroid style)', '仙女风(Fairy style)', '古风(traditional chinese style)', '壮族服装风(Zhuang style)', '欧式田野风(European fields)', '自然户外风(natural outdoor)','武林（gongfu）'
 ]
 
 styles = [
@@ -27,14 +28,38 @@ styles = [
      'multiplier_style': 0.3,
      'multiplier_human': 0.95,
      'add_prompt_style': 'inky cloth, dark hair, Splash ink'},    # 提示词，重要
-    {'name': '水墨2',   
-     'img': './style_image/shuimo.jpg',   
-     'model_id': 'Tyytyy528/ShortSemester2023_zju_02',    
-     'revision': 'v2.0.1',   
-     'bin_file': 'shuimo-000008.safetensors',   
+    {'name': '水墨2',
+     'img': './style_image/shuimo.jpg',
+     'model_id': 'Tyytyy528/ShortSemester2023_zju_02',
+     'revision': 'v2.0.1',
+     'bin_file': 'shuimo-000008.safetensors',
      'multiplier_style': 0.3,
      'multiplier_human': 0.95,
-     'add_prompt_style': 'inky cloth, dark hair, Splash ink'},  
+     'add_prompt_style': 'inky cloth, dark hair, Splash ink'},
+    {'name': '特种兵01',
+     'img': './style_image/special_force.jpg',
+     'model_id': 'Tyytyy528/ShortSemester2023_zju_02',
+     'revision': 'v2.0.2',
+     'bin_file': 'special_force.safetensors',
+     'multiplier_style': 0.3,
+     'multiplier_human': 0.95,
+     'add_prompt_style': 'black military Helmet, black vest, goggles overhead, dark gloves, holding a rifle, buildings'},
+    {'name': '特种兵02',   
+     'img': './style_image/special_force.jpg',   
+     'model_id': 'Tyytyy528/ShortSemester2023_zju_02',    
+     'revision': 'v2.0.2',   
+     'bin_file': 'special_force-000002.safetensors',   
+     'multiplier_style': 0.3,
+     'multiplier_human': 0.95,
+     'add_prompt_style': 'military, goggles overhead, holding a gun, mask'},  
+    {'name': '特种兵03',   
+     'img': './style_image/special_force.jpg',   
+     'model_id': 'Tyytyy528/ShortSemester2023_zju_02',    
+     'revision': 'v2.0.2',   
+     'bin_file': 'special_force-000004.safetensors',   
+     'multiplier_style': 0.3,
+     'multiplier_human': 0.95,
+     'add_prompt_style': 'military uniform, goggles overhead, holding a rifle, buildings, solo'},  
     # end
     {'name': '工作服(Working suit)',
      'img': './style_image/Working_suit.jpg',
@@ -204,6 +229,7 @@ styles = [
      'multiplier_style': 0.86,
      'multiplier_human': 0.95,
      'add_prompt_style': 'girl'},
+    # zju_02 add here
     {'name': '武林风(gongfu)',
      'img': './style_image/Fairy_style.jpg',
      'model_id': 'lljjcc/gongfu',
@@ -212,7 +238,6 @@ styles = [
      'multiplier_style': 0.86,
      'multiplier_human': 0.95,
      'add_prompt_style': '1boy, martial arts, solo, jewelry, long hair, earrings, necklace, shut up, gray eyes, belt, cross, tassel, upper body, lips, tattoos'}
-    
 ]
 
 pose_models = [
