8bca5dbe3f71aa48a737e93493adabd920052a8f
PeiPei233 <2503585731@qq.com>
2023-09-13 17:23:22 +0800

    add tibetan clothing style

--- facechain/constants.py
+++ facechain/constants.py
@@ -13,7 +13,7 @@ base_models = [
     'model_id': 'YorickHe/majicmixRealistic_v6',
     'revision': 'v1.0.0',
     'sub_path': "realistic",
-    'style_list': ['冬季汉服(Chinese winter hanfu)', '校服风(School uniform)', '婚纱风(Wedding dress)', '夜景港风(Hong Kong night style)', '雨夜(Rainy night)', '模特风(Model style)', '机车风(Motorcycle race style)', '婚纱风-2(Wedding dress 2)','拍立得风(Polaroid style)', '仙女风(Fairy style)', '古风(traditional chinese style)', '壮族服装风(Zhuang style)', '欧式田野风(European fields)', '雪山羽绒服风(Jacket in Snow Mountain)', '旗袍风(Cheongsam)']},
+    'style_list': ['冬季汉服(Chinese winter hanfu)', '校服风(School uniform)', '婚纱风(Wedding dress)', '夜景港风(Hong Kong night style)', '雨夜(Rainy night)', '模特风(Model style)', '机车风(Motorcycle race style)', '婚纱风-2(Wedding dress 2)','拍立得风(Polaroid style)', '仙女风(Fairy style)', '古风(traditional chinese style)', '壮族服装风(Zhuang style)', '欧式田野风(European fields)', '雪山羽绒服风(Jacket in Snow Mountain)', '旗袍风(Cheongsam)', '藏族服饰风格(tibetan clothing style)']},
 ]
 
 styles = [
@@ -193,6 +193,14 @@ styles = [
      'multiplier_style': 0.45,
      'multiplier_human': 0.95,
      'add_prompt_style': 'white_cheongsam, photography, warm light, Chinese classical indoor scene, close-up, front view, earrings, hairpin, serenity, elegant, facing the camera with a smile, beautiful chinese embroidery, symmetrical short sleeves'},
+     {'name': '藏族服饰风格(tibetan clothing style)',
+     'img': './style_image/tibetan_clothing.jpg',
+     'model_id': 'PeiPeiY/style_lora',
+     'revision': 'v1',
+     'bin_file': 'tibetan.safetensors',
+     'multiplier_style': 0.7,
+     'multiplier_human': 0.9,
+     'add_prompt_style': 'extremely delicate and beautiful girls, (tibetan clothing style), ((fur hat)), solo, upper_body, raw photo, masterpiece, medium shot, high detail face, slim body, photorealistic, best quality, long hair, colorful makeup, upper_body, raw photo, masterpiece, solo, medium shot, high detail face, slim body, photorealistic, best quality'},
 
 ]
 
