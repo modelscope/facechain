2c5c0000471232f6f12c06615c8d3ad23b52ecc7
SoulNut <jy18785026024@163.com>
2023-09-15 01:23:04 +0800

    modified:   facechain/constants.py 	new file:   style_image/fashionglasses.jpg 	new file:   style_image/miaozu.jpg

--- facechain/constants.py
+++ facechain/constants.py
@@ -1,4 +1,4 @@
-neg_prompt = '(nsfw:2), paintings, sketches, (worst quality:2), (low quality:2), ' \
+neg_prompt = '(nsfw:2), paintings, sketches, (worst quality:2), (low quality:2), malformed hands, missing fingers' \
              'lowers, normal quality, ((monochrome)), ((grayscale)), logo, word, character, bad hand, tattoo, (username, watermark, signature, time signature, timestamp, artist name, copyright name, copyright),'\
              'low res, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, age spot, glans, extra fingers, fewer fingers, strange fingers, bad hand, mole, ((extra legs)), ((extra hands))'
 pos_prompt_with_cloth = 'raw photo, masterpiece, chinese, {}, solo, medium shot, high detail face, looking straight into the camera with shoulders parallel to the frame, slim body, photorealistic, best quality'
@@ -14,7 +14,7 @@ base_models = [
     'model_id': 'YorickHe/majicmixRealistic_v6',
     'revision': 'v1.0.0',
     'sub_path': "realistic",
-    'style_list': ['冬季汉服(Chinese winter hanfu)', '校服风(School uniform)', '婚纱风(Wedding dress)', '夜景港风(Hong Kong night style)', '雨夜(Rainy night)', '模特风(Model style)', '机车风(Motorcycle race style)', '婚纱风-2(Wedding dress 2)','拍立得风(Polaroid style)', '仙女风(Fairy style)', '古风(traditional chinese style)', '壮族服装风(Zhuang style)', '欧式田野风(European fields)', '雪山羽绒服风(Jacket in Snow Mountain)', '旗袍风(Cheongsam)', '日系和服风(Kimono Style)', '贵族公主风(Princess costum)','机械风(Mechanical)','林中鹿女风(deer girl)','漫画风(cartoon)']},
+    'style_list': ['冬季汉服(Chinese winter hanfu)', '校服风(School uniform)', '婚纱风(Wedding dress)', '夜景港风(Hong Kong night style)', '雨夜(Rainy night)', '模特风(Model style)', '机车风(Motorcycle race style)', '婚纱风-2(Wedding dress 2)','拍立得风(Polaroid style)', '仙女风(Fairy style)', '古风(traditional chinese style)', '壮族服装风(Zhuang style)', '欧式田野风(European fields)', '雪山羽绒服风(Jacket in Snow Mountain)', '旗袍风(Cheongsam)', '日系和服风(Kimono Style)', '贵族公主风(Princess costum)','机械风(Mechanical)','林中鹿女风(deer girl)','漫画风(cartoon)','苗族服装风(Miao style)','时尚墨镜风(fashionglasses)']},
 ]
 
 styles = [
@@ -282,6 +282,22 @@ styles = [
      'multiplier_style': 0.25,
      'multiplier_human': 0.95,
      'add_prompt_style': 'purple hair, ponytail, wearing slip skirt,looking straight into the camera with shoulders parallel to the frame, light purple background'},
+    {'name': '苗族服装风(Miao style)',
+     'img': './style_image/miaozu.jpg',
+     'model_id': 'SoulNut/facechain_model',
+     'revision': 'v3.0',
+     'bin_file': 'miaozu.safetensors',
+     'multiplier_style': 0.45,
+     'multiplier_human': 0.9,
+     'add_prompt_style': '(masterpiece, ultra high res face, face ultra zoom, highres, best quality, ultra detailed, portrait:1.2), sfw, Miao nationality, smaller head, beautiful face, smaller face, ((intricate embroideries on clothes)), silver ornaments, ((intricate detailed headdresses)), (((extremely elaborate silver detailing))), exquisite and ornate headpiece featuring intricate filigree designs and delicate engravings, leaning against a stone archway, hands clasped behind, wear gloves, detailed fingers,  Miao Village background, flat chest, realistic'},
+    {'name': '时尚墨镜风(fashionglasses)',
+     'img': './style_image/fashionglasses.jpg',
+     'model_id': 'SoulNut/facechain_LoRA',
+     'revision': 'v1.1',
+     'bin_file': 'fashion_glasses.safetensors',
+     'multiplier_style': 0.6,
+     'multiplier_human': 0.95,
+     'add_prompt_style': '(masterpiece, ultra high res face, face ultra zoom, highres, best quality, ultra detailed, cinematic lighting, portrait:1.2), sfw, short hair, blonde-glasses,solo,close-up,red lips,facing aside, cityscape, detailed background, blonde Leather clothing, blonde theme'},
 ]
 
 pose_models = [
