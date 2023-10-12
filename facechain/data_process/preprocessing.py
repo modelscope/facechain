d83ce77e083e978c76be9059cb5aa92f26353166
Jack <574768706@qq.com>
2023-10-12 15:32:24 +0800

    Feat/sdwebui (#337)
    * integration of facechain into sdwebui
    
    * not ui_settings
    
    * div height update: Styles supported by this base model.
    
    * Update app.py (#347)
    
    * fix the error of "....\\train_text_to_image_lora.py': [Errno 2] No such file or directory" on windows platform
    
    * fix encoding error on windows platform
    
    *  fix "file not found error" of train_text_to_image_lora.py on windows platform
    
    ---------
    
    Co-authored-by: wenmeng.zwm <wenmeng.zwm@alibaba-inc.com>
    Co-authored-by: zhengqilin <zhengqilin>
    Co-authored-by: ultimatech-cn <142652112+ultimatech-cn@users.noreply.github.com>

--- facechain/data_process/preprocessing.py
+++ facechain/data_process/preprocessing.py
@@ -203,6 +203,7 @@ class Blipv2():
     def __init__(self):
         self.model = DeepDanbooru()
         self.skin_retouching = pipeline('skin-retouching-torch', model='damo/cv_unet_skin_retouching_torch', model_revision='v1.0.1')
+        # ToDo: face detection
         self.face_detection = pipeline(task=Tasks.face_detection, model='damo/cv_ddsar_face-detection_iclr23-damofd', model_revision='v1.1')
         # self.mog_face_detection_func = pipeline(Tasks.face_detection, 'damo/cv_resnet101_face-detection_cvpr22papermogface')
         self.segmentation_pipeline = pipeline(Tasks.image_segmentation,
