2dd649e1779b1037e83ca72f3081c41aff112ade
XDUWQ <1300964705@qq.com>
2023-10-26 21:43:40 +0800

    update

--- facechain/inference.py
+++ facechain/inference.py
@@ -129,8 +129,7 @@ def main_diffusion_inference(pos_prompt, neg_prompt,
         style_model_path = os.path.join(model_dir, 'zjz_mj_jiyi_small_addtxt_fromleo.safetensors')
     
     test_model_path = '/mnt/workspace/.cache/modelscope/AI-ModelScope/stable-diffusion-xl-base-1.0'
-    pipe = StableDiffusionXLPipeline.from_pretrained(test_model_path, safety_checker=None, torch_dtype=torch.float32)
-    lora_style_path = style_model_path
+    pipe = StableDiffusionPipeline.from_pretrained(base_model_path, safety_checker=None, torch_dtype=torch.float32)    lora_style_path = style_model_path
     lora_human_path = lora_model_path
     pipe = merge_lora(pipe, lora_style_path, multiplier_style, from_safetensor=True)
     pipe = merge_lora(pipe, lora_human_path, multiplier_human, from_safetensor=lora_human_path.endswith('safetensors'))
