b5df5987426aff9bc28194b0c1c43a036d7c7f3e
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2023-11-10 12:55:35 +0800

    fix app.py command

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def train_lora_fn(base_model_path=None, revision=None, sub_path=None, output_img
     else:
         print(f'** project dir: {project_dir}')
         res = os.system(
-            f'PYTHONPATH=. accelerate launch {project_dir}/facechain/train_text_to_image_lora_sdxl.py ' if base_model_path is SDXL_BASE_MODEL_ID else f'{project_dir}/facechain/train_text_to_image_lora_sdxl.py '
+            f'PYTHONPATH=. accelerate launch {project_dir}/facechain/train_text_to_image_lora_sdxl.py ' if base_model_path is SDXL_BASE_MODEL_ID else f'{project_dir}/facechain/train_text_to_image_lora.py '
             f'--pretrained_model_name_or_path={base_model_path} '
             f'--revision={revision} '
             f'--sub_path={sub_path} '
