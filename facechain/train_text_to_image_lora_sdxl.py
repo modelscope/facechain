3c81cf2b21d56c9a54563a8e0c7750fa1d3d228f
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2023-11-10 18:34:06 +0800

    update

--- facechain/train_text_to_image_lora_sdxl.py
+++ facechain/train_text_to_image_lora_sdxl.py
@@ -1288,52 +1288,52 @@ def main():
     pipeline = DiffusionPipeline.from_pretrained(
         model_dir, torch_dtype=weight_dtype
     )
-
-    if args.use_peft:
-        def load_and_set_lora_ckpt(pipe, ckpt_dir, global_step, device, dtype):
-            with open(os.path.join(args.output_dir, f"{global_step}_lora_config.json"), "r") as f:
-                lora_config = json.load(f)
-            print(lora_config)
-
-            checkpoint = os.path.join(args.output_dir, f"{global_step}_lora.pt")
-            lora_checkpoint_sd = torch.load(checkpoint)
-            unet_lora_ds = {k: v for k, v in lora_checkpoint_sd.items() if "text_encoder_" not in k}
-            text_encoder_lora_ds = {
-                k.replace("text_encoder_", ""): v for k, v in lora_checkpoint_sd.items() if "text_encoder_" in k
-            }
-
-            unet_config = LoraConfig(**lora_config["peft_config"])
-            pipe.unet = LoraModel(unet_config, pipe.unet)
-            set_peft_model_state_dict(pipe.unet, unet_lora_ds)
-
-            if "text_encoder_peft_config" in lora_config:
-                text_encoder_config = LoraConfig(**lora_config["text_encoder_peft_config"])
-                pipe.text_encoder = LoraModel(text_encoder_config, pipe.text_encoder)
-                set_peft_model_state_dict(pipe.text_encoder, text_encoder_lora_ds)
-
-            if dtype in (torch.float16, torch.bfloat16):
-                pipe.unet.half()
-                pipe.text_encoder.half()
-
-            pipe.to(device)
-            return pipe
-
-        pipeline = load_and_set_lora_ckpt(pipeline, args.output_dir, global_step, accelerator.device, weight_dtype)
-    elif args.use_swift:
-        if not is_swift_available():
-            raise ValueError(
-                'Please install swift by `pip install ms-swift` to use efficient_tuners.'
-            )
-        from swift import Swift
-        pipeline = pipeline.to(accelerator.device)
-        pipeline.unet = Swift.from_pretrained(pipeline.unet, os.path.join(args.output_dir, 'swift'))
-
-        if args.train_text_encoder:
-            pipeline.text_encoder = Swift.from_pretrained(pipeline.text_encoder, os.path.join(args.output_dir, 'text_encoder'))
-    else:
-        pipeline = pipeline.to(accelerator.device)
-        # load attention processors
-        pipeline.unet.load_attn_procs(args.output_dir)
+    #
+    # if args.use_peft:
+    #     def load_and_set_lora_ckpt(pipe, ckpt_dir, global_step, device, dtype):
+    #         with open(os.path.join(args.output_dir, f"{global_step}_lora_config.json"), "r") as f:
+    #             lora_config = json.load(f)
+    #         print(lora_config)
+    #
+    #         checkpoint = os.path.join(args.output_dir, f"{global_step}_lora.pt")
+    #         lora_checkpoint_sd = torch.load(checkpoint)
+    #         unet_lora_ds = {k: v for k, v in lora_checkpoint_sd.items() if "text_encoder_" not in k}
+    #         text_encoder_lora_ds = {
+    #             k.replace("text_encoder_", ""): v for k, v in lora_checkpoint_sd.items() if "text_encoder_" in k
+    #         }
+    #
+    #         unet_config = LoraConfig(**lora_config["peft_config"])
+    #         pipe.unet = LoraModel(unet_config, pipe.unet)
+    #         set_peft_model_state_dict(pipe.unet, unet_lora_ds)
+    #
+    #         if "text_encoder_peft_config" in lora_config:
+    #             text_encoder_config = LoraConfig(**lora_config["text_encoder_peft_config"])
+    #             pipe.text_encoder = LoraModel(text_encoder_config, pipe.text_encoder)
+    #             set_peft_model_state_dict(pipe.text_encoder, text_encoder_lora_ds)
+    #
+    #         if dtype in (torch.float16, torch.bfloat16):
+    #             pipe.unet.half()
+    #             pipe.text_encoder.half()
+    #
+    #         pipe.to(device)
+    #         return pipe
+    #
+    #     pipeline = load_and_set_lora_ckpt(pipeline, args.output_dir, global_step, accelerator.device, weight_dtype)
+    # elif args.use_swift:
+    #     if not is_swift_available():
+    #         raise ValueError(
+    #             'Please install swift by `pip install ms-swift` to use efficient_tuners.'
+    #         )
+    #     from swift import Swift
+    #     pipeline = pipeline.to(accelerator.device)
+    #     pipeline.unet = Swift.from_pretrained(pipeline.unet, os.path.join(args.output_dir, 'swift'))
+    #
+    #     if args.train_text_encoder:
+    #         pipeline.text_encoder = Swift.from_pretrained(pipeline.text_encoder, os.path.join(args.output_dir, 'text_encoder'))
+    # else:
+    #     pipeline = pipeline.to(accelerator.device)
+    #     # load attention processors
+    #     pipeline.unet.load_attn_procs(args.output_dir)
 
     # run inference
     if args.seed is not None:
