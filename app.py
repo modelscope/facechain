f18f574c3aaa39d738b452f48fa15354f7dfc442
wenmeng zhou <wenmeng.zwm@alibaba-inc.com>
2023-11-28 14:22:28 +0800

    fix: accelerate not found (#427)
    accelerate may not bye available in some envs in sd-webui,   and in most cases training a lora model only need on gpu, so remove accelerate
    
    Co-authored-by: Xingjun.Wang <xingjun.wxj@alibaba-inc.com>

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def train_lora_fn(base_model_path=None, revision=None, sub_path=None, output_img
             ]
         else:
             command = [
-                'accelerate', 'launch', f'{project_dir}/facechain/train_text_to_image_lora_sdxl.py' if base_model_path is SDXL_BASE_MODEL_ID else f'{project_dir}/facechain/train_text_to_image_lora.py',
+                'python', f'{project_dir}/facechain/train_text_to_image_lora_sdxl.py' if base_model_path is SDXL_BASE_MODEL_ID else f'{project_dir}/facechain/train_text_to_image_lora.py',
                 f'--pretrained_model_name_or_path={base_model_path}',
                 f'--revision={revision}',
                 f'--sub_path={sub_path}',
